152b4fe6d0aafa931bb0401c28f88de494b10f59
John <gogl0l@outlook.com>
2021-07-16 08:49:32 +0200

    Added disabled dev-shm-usage (apperently it seem to be to prevent running chrome as sudo)

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = flask.Flask(__name__)
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_arguments("--disable-dev-shm-usage")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 #chrome_driver = "./resources/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
