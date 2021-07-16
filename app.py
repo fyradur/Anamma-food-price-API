3ba97c81889784e039ec8f8d88569ee112d5cacc
John <gogl0l@outlook.com>
2021-07-16 04:07:53 +0200

    Ready for first deployment

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ app = flask.Flask(__name__)
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
-chrome_driver = "./resources/chromedriver_linux64/chromedriver"
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#chrome_driver = "./resources/chromedriver_linux64/chromedriver"
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
 
